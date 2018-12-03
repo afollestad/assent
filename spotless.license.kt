00832032b5124517b6e771db4afeb5ffe6589bdf
Aidan Follestad <afollestad@squareup.com>
2018-12-03 12:57:05 -0800

    Update license header

--- spotless.license.kt
+++ spotless.license.kt
@@ -13,32 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.afollestad.assent.internal
-
-/** @author Aidan Follestad (afollestad) */
-internal class Queue<T> {
-
-  private var data: MutableList<T> = mutableListOf()
-
-  fun push(item: T) = data.add(item)
-
-  fun poll() = if (data.isNotEmpty()) data[0] else null
-
-  fun pop(): T {
-    val result = poll() ?: throw IllegalStateException("Queue is empty, cannot pop.")
-    data.removeAt(0)
-    return result
-  }
-
-  fun size() = data.size
-
-  fun isEmpty() = data.isEmpty()
-
-  fun isNotEmpty() = data.isNotEmpty()
-
-  fun clear() = data.clear()
-
-  operator fun plusAssign(item: T) {
-    push(item)
-  }
-}
